c42e2fb0bd3eea280d4109b67b37a6e4bac0ebb7
Jake Miller <jake0miller@gmail.com>
2019-07-08 10:20:05 -0600

    Refactor from Ruby to AR

--- app/models/astronaut.rb
+++ app/models/astronaut.rb
@@ -3,16 +3,15 @@ class Astronaut < ApplicationRecord
   has_many :astronaut_missions
   has_many :missions, through: :astronaut_missions
 
-  def self.average_age(astronauts)
-    tot = astronauts.sum {|astro| astro.age}.to_f
-    (tot / astronauts.length).round(1)
+  def self.average_age
+    average(:age)
   end
 
   def sorted_missions
-    self.missions.sort {|a,b| a.title <=> b.title}
+    self.missions.order(:title)
   end
 
   def total_time_in_space
-    self.missions.sum {|mission| mission.time_in_space}
+    self.missions.sum(:time_in_space)
   end
 end
